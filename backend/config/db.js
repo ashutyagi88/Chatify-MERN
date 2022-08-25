da7d32ac581d1880ea37279eb8397957dc2793f5
Ashutosh <ashutyagi88@Ashutoshs-MacBook-Air.local>
2022-08-25 23:24:37 +0530

    make it better

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const express = require("express");
 
 const connectDB = async () => {
-  const app = express();
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
@@ -10,9 +9,6 @@ const connectDB = async () => {
     });
 
     console.log(`MONGO DB Connected ${conn.connection.host}`);
-    app.get("/", (req, res, next) => {
-      res.send("Data Connected");
-    });
   } catch (error) {
     console.log(`Error:${error.message}`);
     process.exit();
