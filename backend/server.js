3d808713bf67f1237f213fd18d79712c8664cc48
Ashutosh <ashutyagi88@Ashutoshs-MacBook-Air.local>
2022-08-25 23:10:24 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,18 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://chatify-mern-app.web.app/"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://chatify-mern-app.web.app/"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 
